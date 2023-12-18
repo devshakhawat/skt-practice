f0db552ec011130b8b478d5fd21ec6cbab98df51
Shakhawat Hossain <shakhawat579@gmail.com>
2023-12-18 14:59:14 +0600

    working

--- includes/hooks.php
+++ includes/hooks.php
@@ -4,6 +4,7 @@ namespace SKTPRAC;
 
 class Hooks {
     public function __construct() {
+        
         add_action( 'plugins_loaded', [ $this, 'plugin_loaded' ] );
     }
 
@@ -17,21 +18,21 @@ class Hooks {
         require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
         $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}frontend_form (
-            id INT(20) unsigned NOT NULL AUTO_INCREMENT,
-            amount int(10),
-            buyer` varchar(255),
-            receipt_id varchar(20),
-            items varchar(255),
-            buyer_email varchar(50) NOT NULL,
-            buyer_ip varchar(20),
-            note text,
-            city varchar(20),
-            phone varchar(20),
-            hash_key varchar(255),
-            entry_at date,
-            entry_by int(10),
+            id INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+            amount INT(10),
+            buyer VARCHAR(255),
+            receipt_id VARCHAR(20),
+            items VARCHAR(255),
+            buyer_email VARCHAR(50) NOT NULL,
+            buyer_ip VARCHAR(20),
+            note TEXT,
+            city VARCHAR(20),
+            phone VARCHAR(20),
+            hash_key VARCHAR(255),
+            entry_at DATE,
+            entry_by INT(10),
             PRIMARY KEY (id)
-            )" . $wpdb->get_charset_collate() . ";";
+        ) {$wpdb->get_charset_collate()}";
 
         if ( get_option("{$wpdb->prefix}frontend_form") < $skt_db_version ) {
             dbDelta( $sql );
